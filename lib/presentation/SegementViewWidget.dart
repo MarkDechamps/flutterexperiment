1a62fd42affe510e401f5b41b6942afed874e819
mrxjn <mark.dechamps@gmail.com>
2018-11-15 11:25:18 +0100

    latest

--- lib/presentation/SegementViewWidget.dart
+++ lib/presentation/SegementViewWidget.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hl7trainer/domain/Answer.dart';
@@ -7,8 +5,6 @@ import 'package:hl7trainer/domain/FieldMetadata.dart';
 import 'package:hl7trainer/domain/SegmentMetadata.dart';
 import 'package:hl7trainer/infrastructure/XmlFieldnamesParser.dart';
 import 'package:hl7trainer/presentation/WidgetUtils.dart';
-//import 'package:material_search/material_search.dart';
-import 'package:material_search_bar/material_search_bar.dart';
 
 class SegmentViewWidget extends StatefulWidget {
   @override
@@ -20,17 +16,26 @@ class SegmentViewWidget extends StatefulWidget {
 class SegmentChooseWidgetState extends State<SegmentViewWidget> {
   Map<String, SegmentMetadata> _fieldMap;
   List<Answer> filteredData = [];
-  Map<String, FieldMetadata> _flattenedSegments;
+  Map<String,FieldMetadata> _flattenedSegments;
+  String previous="not empty to start with";
+  final textController = TextEditingController();
 
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is removed from the Widget tree
+    textController.dispose();
+    super.dispose();
+  }
   @override
   void initState() {
     _fieldMap = XmlFieldnamesParser.getSegments();
-    var flattened = _fieldMap.values
-        .expand((v) => v.fields)
-        .map((f) => MapEntry(f.name + " "+ f.parentType+f.index.toString(), f))
-        .toList();
+     var flattened = _fieldMap.values
+         .expand((v) => v.fields)
+         .map((f)=>MapEntry(f.name.toUpperCase(),f))
+         .toList();
     _flattenedSegments = Map.fromEntries(flattened);
     filteredData.add(new Answer("empty"));
+    textController.addListener(filter);
     super.initState();
   }
 
@@ -43,78 +48,54 @@ class SegmentChooseWidgetState extends State<SegmentViewWidget> {
   }
 
   Widget body(BuildContext context) {
-   return enterSearchArg();
+    return new Card(
+        child: SizedBox(
+      height: 400.0,
+      child: Scrollbar(
+        child: new ListView(children: <Widget>[
+          new Card(child: enterSearchArg()),
+          possibleAnswers(filteredData)
+        ]),
+      ),
+    ));
   }
 
   Widget enterSearchArg() {
-    MaterialSearchBar msb= MaterialSearchBar(
-      searchBarColor: Colors.white,
-      searchBarTextColor: Colors.black,
-      searchBarFontSize: 20.0,
-      searchResultsBackgroundColor: Colors.white,
-      searchResultsTextColor: Colors.black,
-      searchResultsFontSize: 20.0,
-      checkmarkIcon: Icon(Icons.check, color: Colors.teal[400]),
-      /*submitButton: FloatingActionButton(
-        heroTag: null,
-        tooltip: "Search",
-        child: new Icon(Icons.arrow_forward),
-        backgroundColor: Theme.of(context).primaryColor,
-        foregroundColor: Colors.white, onPressed: () {},
-      ),*/
-      items: _flattenedSegments.keys.toList(),
-    );
-    msb.changeOnSubmit(() {
-      for (String s in msb.selectedItems) print(s);
-    });
-    msb.changeOnButtonSubmit((String value) {
-      for (String s in msb.selectedItems) print(s);
-    });
-
-    return msb;
+    return TextField(
+        //onChanged: (input) => filterOn(input),
+        controller: textController,
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+            labelText: "Type your search argument",
+            border: InputBorder.none,
+            hintText: 'Enter txt'));
   }
 
-  /*Widget enterSearchArg() {
-    return new MaterialSearchInput<String>(
-      placeholder: 'Search',
-      formatter: (t)=> t+"::",
-      getResults: (String criteria) async {
-        var list = await filterOn(criteria);
-        return list
-            .map((result) => new MaterialSearchResult<String>(
-                  value: result.answer, //The value must be of type <String>
-                  text: result.answer, //String that will be show in the list
-                  icon: Icons.question_answer,
-                ))
-            .toList();
-      },
-      filter: (dynamic value, String criteria) {
-        return value.toLowerCase().trim()
-            .contains(new RegExp(r'' + criteria.toLowerCase().trim() + ''));
-      },
-      //  results:filterOn(criteria)
-    );
-  }*/
-
-  Future<List<Answer>> filterOn(String input) {
-    if (input.isEmpty) return Future.sync(() => []);
-
-    //setState(() {
-    return Future.sync(() => filterAnswers(input));
-    //});
+  void filter(){
+    //print("Input:"+textController.text);
+    filterOn(textController.text);
   }
 
-  List<Answer> filterAnswers(String input) {
-    return _flattenedSegments.keys
-          .where((s) => s.contains(input.toUpperCase()))
-          .map((s) {
-        var flattenedSegment = _flattenedSegments[s];
-        return Answer(flattenedSegment.name +
-            ": " +
-            flattenedSegment.parentType +
-            "^" +
-            flattenedSegment.index.toString());
-      }).toList();
+  void filterOn(String input) {
+    input = input.toUpperCase();
+    if (input.isEmpty || input==previous) return;
+    previous = input;
+    print("[$input]");
+    var where = _flattenedSegments.keys
+        .where((s) => s.contains(input.toUpperCase()));
+    setState(() {
+
+    //Stopwatch stopwatch = new Stopwatch()..start();
+    ///print("mapping...------------------- START");
+
+    filteredData = where
+        .map((s) {
+      //print("mapping..."+s);
+      var flattenedSegment = _flattenedSegments[s];
+      return Answer(flattenedSegment.name + ": " + flattenedSegment.parentType + "^" + flattenedSegment.index.toString());
+    }).toList();
+    //print("mapping...------------------- STOP ${stopwatch.elapsed}");
+    });
   }
 
   possibleAnswers(List<Answer> answers) {
